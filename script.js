d885335f592aaa5e46fd72624b0069d90a40c0aa
Joaquin Nievas <joacokavra@gmail.com>
2023-09-15 01:01:26 -0400

    amogu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("working tbh")
+
 if (navigator.userAgent.toLowerCase().includes("discord")) {
     window.location.href = "https://cdn.discordapp.com/attachments/875551171690000429/1152105638407716894/DontMidClick.gif";
 } else {
