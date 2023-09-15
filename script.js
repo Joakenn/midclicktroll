d35b22bb5c9396bfe07323affaa5433ba50c42ce
Joaquin Nievas <joacokavra@gmail.com>
2023-09-15 00:58:54 -0400

    attempting to add redirection

---
+++ script.js
@@ -0,0 +1,5 @@
+if (navigator.userAgent.toLowerCase().includes("discord")) {
+    window.location.href = "https://cdn.discordapp.com/attachments/875551171690000429/1152105638407716894/DontMidClick.gif";
+} else {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
